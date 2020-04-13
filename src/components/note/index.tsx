051aacf65886d89f18c3b28c5f84731740ed5820
John Gill <johnagill95@gmail.com>
2020-04-13 17:19:22 +0100

    fixed the spacing issues on messages

--- src/components/note/index.tsx
+++ src/components/note/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, useWindowDimensions } from 'react-native';
+import { Text } from 'native-base';
+
 import _ from 'lodash';
 import { NoteType } from '../../context/notesContext';
 
@@ -26,33 +28,30 @@ const styles = StyleSheet.create({
 });
 
 export default ({ note, index, onPress }: NoteProps) => {
-  if (note.display) {
-    return (
-      <TouchableOpacity
-        style={[
-          styles.noteContainer,
-          {
-            top: note.location.y,
-            left: note.location.x,
-          },
-        ]}
-        onPress={(): void => onPress(index)}>
-        <Text style={{ color: 'white' }}>{note.value}</Text>
-      </TouchableOpacity>
-    );
-  }
+  const [spacingNeeded, setSpacingNeeded] = useState<number>(note.location.x);
+  const windowWidth = useWindowDimensions().width;
+
+  const calculateDisplaySpacing = (left: number, textSize: number) => {
+    if (windowWidth - left > textSize) {
+      return left;
+    }
+    if (windowWidth - left > 60) {
+      return setSpacingNeeded(5);
+    }
+    setSpacingNeeded(left - textSize);
+  };
   return (
     <TouchableOpacity
       style={[
         styles.noteContainer,
-        styles.noteContainerIndex,
         {
           top: note.location.y,
-          left: note.location.x,
+          left: spacingNeeded,
         },
       ]}
-      onPress={(): void => onPress(index)}>
-      <Text style={{ color: 'white' }}>{index + 1}</Text>
+      onPress={(): void => onPress(index)}
+      onLayout={(e) => (note.display ? calculateDisplaySpacing(note.location.x, e.nativeEvent.layout.width) : setSpacingNeeded(note.location.x))}>
+      <Text style={{ color: 'white', flex: 1, flexWrap: 'wrap' }}>{!note.display ? index + 1 : note.value}</Text>
     </TouchableOpacity>
   );
 };
